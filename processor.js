0521ade68d51e7823204b8369678b45b9510e5d2
lucas3d4783 <lucas3d4783@gmail.com>
2021-01-02 19:27:03 -0300

    Adicionando teste para verificar se já não foi adicionado o estilo escolhido no texto selecionado

--- processor.js
+++ processor.js
@@ -23,13 +23,17 @@ function concatenar(init, end, message){
     
     console.log("Posição inicial selecionada: " + ta.selectionStart);
     console.log("Posição final selecionada: " + ta.selectionEnd);
-    console.log("Valor selecionado" + ta.value.substring(ta.selectionStart, ta.selectionEnd));
 
-    if(ta.value.substring(ta.selectionStart, ta.selectionEnd) != ""){
-        var text = ta.value.substring(ta.selectionStart, ta.selectionEnd);
+    var value = ta.value.substring(ta.selectionStart, ta.selectionEnd);
+    console.log("Valor selecionado: " + value);
+
+    if(ta.value.substring(ta.selectionStart, ta.selectionEnd) != ""){ // varifica se foi digitado algo
+        if(!value.includes(init)){ // verifica se já possui o init na string
+            var text = ta.value.substring(ta.selectionStart, ta.selectionEnd);
             ta.value = ta.value.substring(0, ta.selectionStart) + init + text +
-            end + ta.value.substring(ta.selectionEnd);
-        console.log(message);
+                end + ta.value.substring(ta.selectionEnd);
+            console.log(message);
+        }
     }else{
         console.log('Nenhum valor selecionado');
     }
@@ -47,30 +51,31 @@ function italico(){ // transformar seleção em Itálico
     concatenar('<i>','</i>', 'Texto definido como Itálico');
 }
 
-function h1(){ // transformar seleção em Itálico
+function h1(){ // transformar seleção em Título 1
     concatenar('<h1>','</h1>', 'Texto definido como Título 1');
 }
 
-function h2(){ // transformar seleção em Itálico
+function h2(){ // transformar seleção em Título 2
     concatenar('<h2>','</h2>', 'Texto definido como Título 2');
 }
 
-function h3(){ // transformar seleção em Itálico
+function h3(){ // transformar seleção em Título 3
     concatenar('<h3>','</h3>', 'Texto definido como Título 3');
 }
 
-function textleft(){ // transformar seleção em Itálico
+function textleft(){ // Alinhando texto à esquerda
     concatenar('<div class="text-left">','</div>', 'Alinhando texto à esquerda');
 }
 
-function textcenter(){ // transformar seleção em Itálico
+function textcenter(){ // Centralizando o texto
     concatenar('<div class="text-center">','</div>', 'Alinhando texto no centro');
 }
 
-function textright(){ // transformar seleção em Itálico
+function textright(){ // Alinhando texto à direita
     concatenar('<div class="text-right">','</div>', 'Alinhando texto à direita');
 }
-function textjustify(){ // transformar seleção em Itálico
+
+function textjustify(){ // Justificando o texto
     concatenar('<div class="text-justify">','</div>', 'Justificando texto');
 }
 
